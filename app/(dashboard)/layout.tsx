4f5911695831c51fdbda233d7b1b240f7ed03e47
Douglas Reis <contactgestorvip+github@gmail.com>
2025-09-18 16:14:25 -0400

    Adjust dashboard footer spacing

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -27,7 +27,7 @@ const DashboardLayout = async ({ children }: DashboardProps) => {
       <div id="content" className="position-relative min-vh-100 d-flex flex-column">
         <Header user={user} />
         <div className="custom-container py-4 flex-grow-1">{children}</div>
-        <footer className="custom-container pb-4 text-secondary text-center">
+        <footer className="custom-container mt-auto py-3 text-secondary text-center">
           StoreBot Dashboard © {currentYear}. Todos os direitos reservados.
         </footer>
       </div>
